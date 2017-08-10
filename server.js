279bad8ce1672dad86b3d3d6a8cd3b38fe4d72be
Nishal Narechania <nishal@Nishals-MacBook-Pro.local>
2017-08-10 14:04:08 -0400

    updated server.js

--- server.js
+++ server.js
@@ -1,18 +1,16 @@
-var express = require('express');
-var app = express();
+const express = require('express')  
+const app = express()  
+const port = 3000
+const path = require('path')
 
-app.set('port', (process.env.PORT || 5000));
+app.get('/', (request, response) => {  
+  response.sendFile(path.join(__dirname + '/index.html'))
+})
 
-app.use(express.static(__dirname + '/public'));
+app.listen(port, (err) => {  
+  if (err) {
+    return console.log('something bad happened', err)
+  }
 
-// views is directory for all template files
-app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
-
-app.get('/', function(request, response) {
-  response.render('pages/index');
-});
-
-app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
-});
+  console.log(`server is listening on ${port}`)
+})
